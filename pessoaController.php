bec6f0ef87c7ef3689c3f45d6459b47be7ea8175
lezio09 <144951527+lezio09@users.noreply.github.com>
2025-02-10 01:29:40 -0300

    pessoaController alterado

---
+++ pessoaController.php
@@ -0,0 +1,37 @@
+<?php
+    include_once("db.php");
+    include_once("pessoaDAO.php");
+
+    if(isset($_POST["acao"]) && $_POST["acao"] == "entrar"){
+        if(isset($_POST["usuario"]) && isset($_POST["senha"])){
+            $nome = $_POST["usuario"];
+            $senha = $_POST["senha"];
+            save($nome,$senha);
+            header("Location: listagem.php");
+        }else{
+            echo "Erro";
+        }
+    }
+    if(isset($_POST["acao"]) && $_POST["acao"] == "editar"){
+        if(isset($_POST["usuario"]) && isset($_POST["senha"])){
+            $nome = $_POST["usuario"];
+            $senha = $_POST["senha"];
+            save($nome, $senha);
+            header("Location: listagem.php");  
+        }else{
+            echo "Erro ao altetar usuario!"
+        }
+    if(isset($_POST["acao"]) && $_POST["acao"] == "remover"){
+        if(isset($_POST["usuario"]) && isset($_POST["senha"])){
+            $nome = $_POST["usuario"];
+            $senha = $_POST["senha"];
+            save($nome, $senha);
+            header("Location: listagem.php");
+        }else{
+            echo "Erro ao remover usuario!"
+        }
+    }
+
+    }
+
+?>
