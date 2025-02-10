e6da4957f3a6aecc313477fcf84031a37128ddaf
lezio09 <144951527+lezio09@users.noreply.github.com>
2025-02-10 01:13:07 -0300

    Remover

---
+++ deletePessoa.php
@@ -0,0 +1,16 @@
+<?php
+include_once("bd.php");
+include_once("pessoaController");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Remover</title>
+</head>
+<body>
+    <h1>Delet</h1>
+    
+</body>
+</html>
