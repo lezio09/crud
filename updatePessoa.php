5bce84e05cebc1a5fc44bc4733ad769416c61af6
lezio09 <144951527+lezio09@users.noreply.github.com>
2025-02-10 01:12:46 -0300

    Alterar

---
+++ updatePessoa.php
@@ -0,0 +1,15 @@
+<?php
+include_once("bd.php");
+include_once("pessoaController");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alterar</title>
+</head>
+<body>
+    
+</body>
+</html>
